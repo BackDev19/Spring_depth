2ca156a1de663b2a2e958f5724d1989a39dbf95f
BackDev19 <ghkdrnjs180@naver.com>
2022-08-01 19:08:00 +0900

    4-7 Spring Data JPA 이해

--- jpa/src/main/java/com/sparta/jpa/repository/UserRepository.java
+++ jpa/src/main/java/com/sparta/jpa/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.sparta.jpa.repository;
 import com.sparta.jpa.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<Users, String> {
+    List<Users> findByFavoriteFoodContaining(String word);
 }
